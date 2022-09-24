e7afbc4b08fd209ea53764e82a458108f27034ec
Poli <pr@polisystems.ch>
2022-09-24 11:10:48 +0200

    Update VoteBox.js

--- public/js/boxes/VoteBox.js
+++ public/js/boxes/VoteBox.js
@@ -68,7 +68,7 @@ VoteBox.prototype.update = function() {
 }
 
 VoteBox.addButtons = function(sidebar) {
-    sidebar.addButton(new Button('VoteBox', 'Start a vote'));
+    sidebar.addButton(new Button('VoteBox', 'Démarrer un vote'));
 
     //add an event to the submit button of the popup
     var popup = $('#VoteBox-Popup');
