05f562f95bcc57d7d1797f1848297486b77f8a31
Poli <pr@polisystems.ch>
2023-10-31 00:11:06 +0100

    Update db_jvlan_T_Users_2023-10-30_163242.sql

--- db_jvlan_T_Users_2023-10-30_163242.sql
+++ db_jvlan_T_Users_2023-10-30_163242.sql
@@ -1,51 +1,33 @@
--- MySQL dump 10.13  Distrib 8.0.34, for Linux (x86_64)
---
--- Host: 127.0.0.1    Database: db_jvlan
--- ------------------------------------------------------
--- Server version	8.0.34-0ubuntu0.22.04.1
+-- --------------------------------------------------------
+-- Hôte:                         wings.jvlan.ch
+-- Version du serveur:           10.6.12-MariaDB-0ubuntu0.22.04.1 - Ubuntu 22.04
+-- SE du serveur:                debian-linux-gnu
+-- HeidiSQL Version:             12.5.0.6677
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `T_Users`
---
-
-DROP TABLE IF EXISTS `T_Users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `T_Users` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
-  `login` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `points` int unsigned DEFAULT '0',
-  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'steam_authentification_used',
+-- Listage de la structure de la table s12_jvlan. T_Users
+CREATE TABLE IF NOT EXISTS `T_Users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+  `login` varchar(255) NOT NULL,
+  `points` int(10) unsigned DEFAULT 0,
+  `password` varchar(255) NOT NULL DEFAULT 'steam_authentification_used',
   `IsAdmin` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=5;
 
---
--- Dumping data for table `T_Users`
---
+-- Les données exportées n'étaient pas sélectionnées.
 
-/*!40000 ALTER TABLE `T_Users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `T_Users` ENABLE KEYS */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-10-30 16:32:45
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
