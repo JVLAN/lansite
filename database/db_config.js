407bfc3e248490dbfd94666751df180cfc7c738d
Poli <pr@polisystems.ch>
2023-11-01 02:21:24 +0100

    Fix MySQL crash - use pools instead

--- database/db_config.js
+++ database/db_config.js
@@ -1,11 +1,9 @@
 const mysql = require('mysql2');
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
 	host : 'localhost',
 	user : 'naka',
 	password : 'chien',
 	database : 'db_jvlan'
 });
-
-connection.connect()
 
 module.exports = connection
